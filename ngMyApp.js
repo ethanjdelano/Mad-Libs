c85de66e577dfa6c4d5d7c92ffdd7f77ebe7ee6b
ethanjdelano <ethanjdelano@gmail.com>
2014-06-04 16:28:50 +0100

    controller fixed

---
+++ ngMyApp.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+angular.module('myApp', [])
+	.controller('MyCtrl', function( $scope) {
+			$scope.info = {
+			adjective1: 'fun',
+			adjective2: 'crazy',
+			noun1: 'forest',
+			pluralNoun1: 'horses',
+			game1: 'tag',
+			pluralNoun2: 'rabbits',
+			INGverb1: 'hiking',
+			INGverb2: 'swimming',
+			pluralNoun3: 'meals',
+			noun2: 'golf',
+			INGverb3: 'relaxing',
+			noun3: 'gorge',
+			plant1: 'poison ivy',
+			bodyPart1: 'arm',
+			place1: 'beach',
+			INGverb4: 'running',
+			adjective3: 'tired',
+			number1: '8',
+			pluralNoun4: 'dollars'
+			}
+  });
